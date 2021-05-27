e60f5d064df4587a3810505c7509d9542211fc37
Haitham Shahin <42976403+haithamshahin333@users.noreply.github.com>
2021-05-27 10:55:05 -0400

    updated to be dynamic email by env var

--- Controllers/GraphController.cs
+++ Controllers/GraphController.cs
@@ -62,7 +62,7 @@ namespace GraphApi.Controllers
             // Create a new instance of GraphServiceClient with the authentication provider.
             GraphServiceClient graphClient = new GraphServiceClient(authProvider);
 
-            var user = await graphClient.Users["haitham@aaronbadev.onmicrosoft.com"].Request().GetAsync();
+            var user = await graphClient.Users["USER_EMAIL_QUERY"].Request().GetAsync();
 
             return new JsonResult(user);
         }
